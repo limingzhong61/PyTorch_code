ef2748134af8e68a99570d4f0e8165ac6ad2789b
codeofli <MarsLee5@foxmail.com>
2022-07-02 23:51:03 +0800

    add multiple dimension input

---
+++ Multiple_Dimension_Input/diabetes_prediction.py
@@ -0,0 +1,47 @@
+import torch
+import numpy as np
+
+xy = np.loadtxt('diabetes.csv.gz', delimiter=',', dtype=np.float32)
+
+x_data = torch.from_numpy(xy[:, : -1])
+# xy[:, [1]]是为了取原数据最后一列并且形成矩阵
+y_data = torch.from_numpy(xy[:, [1]])
+# print(y_data.shape)
+# print(y_data)
+
+
+import torch
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.linear1 = torch.nn.Linear(8, 6)
+        self.linear2 = torch.nn.Linear(6, 4)
+        self.linear3 = torch.nn.Linear(4, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.sigmoid(self.linear1(x))
+        x = self.sigmoid(self.linear2(x))
+        x = self.sigmoid(self.linear3(x))
+        return x
+
+
+model = Model()
+
+criterion = torch.nn.BCELoss(reduction='mean')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+for epoch in range(100):
+    # Forward
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    # Backward
+    optimizer.zero_grad()
+    loss.backward()
+
+    # Update
+    optimizer.step()
---
+++ logistic_regression.py
@@ -0,0 +1,47 @@
+import torch
+
+# ①Prepare dataset
+x_data = torch.Tensor([[1.0], [2.0], [3.0]])
+y_data = torch.Tensor([[0], [0], [1]])
+
+
+# ②Design model using Class inherit from nn.Module
+class LogisticRegressionModel(torch.nn.Module):
+    def __init__(self):
+        super(LogisticRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = torch.sigmoid(self.linear(x))
+        return y_pred
+
+
+model = LogisticRegressionModel()
+# --------------------------------------------------------------#
+# ③Construct loss and optimizer using PyTorch API
+criterion = torch.nn.BCELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+# --------------------------------------------------------------#
+# ④Training cycle forward, backward, update
+for epoch in range(1000):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 200)
+x_t = torch.Tensor(x).view((200, 1))
+y_t = model(x_t)
+y = y_t.data.numpy()
+plt.plot(x, y)
+plt.plot([0, 10], [0.5, 0.5], c='r')
+plt.xlabel('Hours')
+plt.ylabel('Probability of Pass')
+plt.grid()
+plt.show()
